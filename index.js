4bea42981a351cd0b580b6c4cec64e470745c38b
belroldan <belenroldaninfo@gmail.com>
2023-10-29 18:54:37 -0300

    few changes

--- index.js
+++ index.js
@@ -1,8 +1,3 @@
-// let inicial = document.getElementById("total").value;
-// let percentage = document.getElementById("percentage").value;
-// let button = document.getElementById("")
-
-
 function calculate(){
     var inicial = parseFloat(document.getElementById("total").value);
     var percentage = parseFloat(document.getElementById("percentage").value);
